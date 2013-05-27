7ac076ca6ee6b94812d43ee71c61d647d7abdac3
Stephan Rave <stephanrave@uni-muenster.de>
2013-05-27 14:04:13 +0200

    [discretizations] add InstationaryNonlinearDiscretization

---
+++ src/pymor/discretizations/nonlinear.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# This file is part of the pyMor project (http://www.pymor.org).
+# Copyright Holders: Felix Albrecht, Rene Milk, Stephan Rave
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+
+from pymor.algorithms.timestepping import explicit_euler
+from pymor.la.interfaces import VectorArray
+from pymor.tools import dict_property
+from pymor.operators import OperatorInterface, LinearOperatorInterface, ConstantOperator
+from pymor.discretizations.interfaces import DiscretizationInterface
+
+
+class InstationaryNonlinearDiscretization(DiscretizationInterface):
+
+    disable_logging = False
+    operator = dict_property('operators', 'operator')
+    rhs = dict_property('operators', 'rhs')
+    initial_data = dict_property('operators', 'initial_data')
+
+    def __init__(self, operator, rhs, initial_data, T, nt, visualizer=None, name=None):
+        assert isinstance(operator, OperatorInterface)
+        assert isinstance(rhs, LinearOperatorInterface)
+        assert isinstance(initial_data, (VectorArray, ConstantOperator))
+        if isinstance(initial_data, VectorArray):
+            initial_data = ConstantOperator(initial_data, name='initial_data')
+        assert operator.dim_source == operator.dim_range == rhs.dim_source == initial_data.dim_range
+        assert rhs.dim_range == 1
+
+        super(InstationaryNonlinearDiscretization, self).__init__()
+        self.operators = {'operator': operator, 'rhs': rhs, 'initial_data': initial_data}
+        self.build_parameter_type(inherits={'operator': operator, 'rhs': rhs, 'initial_data': initial_data},
+                                  provides={'_t': 0})
+        self.T = T
+        self.nt = nt
+
+        if visualizer is not None:
+            self.visualize = visualizer
+
+        self.solution_dim = operator.dim_range
+        self.name = name
+
+    def with_projected_operators(self, operators):
+        assert set(operators.keys()) == {'operator', 'rhs', 'initial_data'}
+        return InstationaryNonlinearDiscretization(**operators)
+
+    def _solve(self, mu=None):
+        mu_A = self.map_parameter(mu, 'operator', provide={'_t': np.array(0)})
+        mu_F = self.map_parameter(mu, 'rhs', provide={'_t': np.array(0)})
+        U0 = self.initial_data.apply(0, self.map_parameter(mu, 'initial_data'))
+        return explicit_euler(self.operator, self.rhs, U0, 0, self.T, self.nt, mu_A, mu_F)

b4aa01bf4a6f92ee246131b8141d48cc2863af19
Stephan Rave <stephanrave@uni-muenster.de>
2013-05-31 13:57:18 +0200

    [discretizers.advection] assemble l2-product and use visualize_glumpy_patch

--- src/pymor/discretizers/advection.py
+++ src/pymor/discretizers/advection.py
@@ -10,9 +10,10 @@ import numpy as np
 
 from pymor.analyticalproblems.advection import InstationaryAdvectionProblem
 from pymor.domaindiscretizers import discretize_domain_default
-from pymor.operators.fv import NonlinearAdvectionLaxFriedrichs
+from pymor.operators.fv import NonlinearAdvectionLaxFriedrichs, L2Product
 from pymor.operators import NumpyLinearOperator
 from pymor.grids import RectGrid
+from pymor.gui.qt import visualize_glumpy_patch
 from pymor.discretizations import InstationaryNonlinearDiscretization
 from pymor.la import induced_norm
 from pymor.la import NumpyVectorArray
@@ -42,9 +43,9 @@ def discretize_instationary_advection_fv(analytical_problem, diameter=None, nt=1
     I = NumpyVectorArray(I)
 
     def visualize(U):
-        assert isinstance(grid, RectGrid)
-        assert len(U) == 1
-        grid.visualize(U.data.ravel())
+        visualize_glumpy_patch(grid, U, bounding_box=grid.domain, codim=0)
+        # assert len(U) == 1
+        # grid.visualize(U.data.ravel())
         # import matplotlib.pyplot as pl
         # pl.tripcolor(grid.centers(2)[:, 0], grid.centers(2)[:, 1], grid.subentities(0, 2), U.data.ravel())
         # pl.colorbar()
@@ -52,6 +53,9 @@ def discretize_instationary_advection_fv(analytical_problem, diameter=None, nt=1
 
     discretization = InstationaryNonlinearDiscretization(L, F, I, p.T, nt, visualizer=visualize,
                                                          name='{}_FV'.format(p.name))
+
+    discretization.l2_product = L2Product(grid)
+    discretization.l2_norm = induced_norm(discretization.l2_product)
 
     if hasattr(p, 'parameter_space'):
         discretization.parameter_space = p.parameter_space

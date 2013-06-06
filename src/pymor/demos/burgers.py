b8cd14b239fef3cb86def1f8c1ab3f9a47a19bc7
Stephan Rave <stephanrave@uni-muenster.de>
2013-06-06 19:53:53 +0200

    [demos] update burgers demo

--- src/pymor/demos/burgers.py
+++ src/pymor/demos/burgers.py
@@ -6,16 +6,22 @@
 '''Thermalblock demo.
 
 Usage:
-  burgers.py [-hp] [--grid=NI] [--nt=COUNT] [--plot-solutions] [--test=COUNT]
-             EXP_MIN EXP_MAX SNAPSHOTS RBSIZE
+  burgers.py [-hp] [--grid=NI] [--nt=COUNT] [--plot-solutions] [--plot-ei-err] [--test=COUNT]
+             EXP_MIN EXP_MAX EI_SNAPSHOTS EISIZE SNAPSHOTS RBSIZE
 
 
 Arguments:
-  EXP_MIN    Minimal exponent
-  EXP_MAX    Maximal exponent
-  SNAPSHOTS  Number of snapshots for basis generation.
+  EXP_MIN       Minimal exponent
 
-  RBSIZE     Size of the reduced basis
+  EXP_MAX       Maximal exponent
+
+  EI_SNAPSHOTS  Number of snapshots for empirical interpolation.
+
+  EISIZE        Number of interpolation DOFs.
+
+  SNAPSHOTS     Number of snapshots for basis generation.
+
+  RBSIZE        Size of the reduced basis
 
 
 Options:
@@ -26,6 +32,8 @@ Options:
   -h, --help             Show this message.
 
   -p, --plot-err         Plot error.
+
+  --plot-ei-err          Plot empirical interpolation error.
 
   --plot-solutions       Plot some example solutions.
 
@@ -50,6 +58,10 @@ from pymor.discretizers.advection import discretize_instationary_advection_fv
 from pymor.reductors import reduce_generic_rb
 from pymor.algorithms import greedy
 from pymor.algorithms.basisextension import pod_basis_extension
+from pymor.algorithms.ei import interpolate_operators
+from pymor.la import NumpyVectorArray
+
+
 core.getLogger('pymor.algorithms').setLevel('INFO')
 core.getLogger('pymor.discretizations').setLevel('INFO')
 
@@ -59,11 +71,12 @@ def burgers_demo(args):
     args['--grid'] = int(args['--grid'])
     args['EXP_MIN'] = int(args['EXP_MIN'])
     args['EXP_MAX'] = int(args['EXP_MAX'])
+    args['EI_SNAPSHOTS'] = int(args['EI_SNAPSHOTS'])
+    args['EISIZE'] = int(args['EISIZE'])
     args['SNAPSHOTS'] = int(args['SNAPSHOTS'])
     args['RBSIZE'] = int(args['RBSIZE'])
     args['--test'] = int(args['--test'])
 
-    print('Solving on RectGrid(({0},{0}))'.format(args['--grid']))
 
     print('Setup Problem ...')
     problem = BurgersProblem(parameter_range=(args['EXP_MIN'], args['EXP_MAX']))
@@ -71,6 +84,7 @@ def burgers_demo(args):
     print('Discretize ...')
     discretization, _ = discretize_instationary_advection_fv(problem, diameter=m.sqrt(2) / args['--grid'],
                                                              nt=args['--nt'])
+    print(discretization.operator.grid)
 
     print(discretization.parameter_info())
 
@@ -83,13 +97,43 @@ def burgers_demo(args):
             discretization.visualize(U)
 
 
+    ei_discretization, ei_data = interpolate_operators(discretization, 'operator',
+                                                       discretization.parameter_space.sample_uniformly(args['EI_SNAPSHOTS']),
+                                                       error_norm=discretization.l2_norm,
+                                                       target_error=1e-10,
+                                                       max_interpolation_dofs=args['EISIZE'])
+
+    if args['--plot-ei-err']:
+        print('Showing some EI errors')
+        for mu in discretization.parameter_space.sample_randomly(2):
+            print('Solving for exponent = \n{} ... '.format(mu['exponent']))
+            sys.stdout.flush()
+            U = discretization.solve(mu)
+            U_EI = ei_discretization.solve(mu)
+            ERR = U - U_EI
+            print('Error: {}'.format(np.max(discretization.l2_norm(ERR))))
+            discretization.visualize(ERR)
+
+        print('Showing interpolation DOFs ...')
+        U = np.zeros(U.dim)
+        dofs = ei_discretization.operator.interpolation_dofs
+        U[dofs] = np.arange(1, len(dofs) + 1)
+        U[ei_discretization.operator.source_dofs] += int(len(dofs)/2)
+        discretization.visualize(NumpyVectorArray(U))
+
+
     print('RB generation ...')
+    import cProfile, pstats, io
 
     reductor = reduce_generic_rb
     extension_algorithm = partial(pod_basis_extension)
-    greedy_data = greedy(discretization, reductor, discretization.parameter_space.sample_uniformly(args['SNAPSHOTS']),
+    # pr = cProfile.Profile()
+    # pr.enable()
+    greedy_data = greedy(ei_discretization, reductor, discretization.parameter_space.sample_uniformly(args['SNAPSHOTS']),
                          use_estimator=False, error_norm=lambda U: np.max(discretization.l2_norm(U)),
                          extension_algorithm=extension_algorithm, max_extensions=args['RBSIZE'])
+    # pr.disable()
+    # pr.dump_stats('greedy.profile')
     rb_discretization, reconstructor = greedy_data['reduced_discretization'], greedy_data['reconstructor']
 
 
